66a8d196fdf7705a96f2b13c9e21d029ec4c9891
arvalaan <9444629+arvalaan@users.noreply.github.com>
2026-03-03 13:23:00 +0100

    fix: align joi package to resolve swagger schema version conflict

--- workers/api.js
+++ workers/api.js
@@ -3,7 +3,7 @@
 const { parentPort } = require('worker_threads');
 const Hapi = require('@hapi/hapi');
 const Boom = require('@hapi/boom');
-const Joi = require('@hapi/joi');
+const Joi = require('joi');
 const logger = require('../lib/logger');
 const hapiPino = require('hapi-pino');
 const { ImapFlow } = require('imapflow');
